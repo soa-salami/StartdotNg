48935e800983c067bd707a2090f614532eae1761
Salami Saheed Olamide <52781881+sosalami19@users.noreply.github.com>
2019-09-05 18:05:38 +0100

    Update saheed.js

--- saheed.js
+++ saheed.js
@@ -3,17 +3,21 @@ function validate(){
     if(name === null || name === ""){
         alert("Name can't be blank");
         return false;
-    } else if (name.length <= 3){
+    } else if (name.length < 4){
         alert("Name must be at least 4 character long");
         return false;
     }
+}
+function validate(){
+    var email = document.contactForm.email.value;
+    return /^[^\s@]+@[^\s@]+\.[^\s@]+/.test(email);
 }
 function validate(){
     var message = document.contactForm.message.value;
     if(message === null || message === ""){
         alert("Message can't be blank");
         return false;
-    } else if (message.length <= 20){
+    } else if (message.length < 20){
         alert("Message must be at least 20 character long")
         return false;
     }
